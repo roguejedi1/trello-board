1a041bc61d5727b0460e8e26607856caf4a9d0cd
Kanoj Vora <kkvsith@gmail.com>
2022-05-03 13:34:13 +0100

    exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import PropTypes from "prop-types"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const TrelloBoard = ({tasks}) => {
+  return (
+    <>
+      <div className="border">
+      <h1 className="title">Task list</h1>
+      <div className="inner">
+      {tasks.map(task => (
+        <div className="task" key={task.id}>
+          <p>{task.name}</p>
+        </div>
+      ))}
+      <input placeholder="Add a task"/>
+      </div>
+      </div>
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+TrelloBoard.propTypes = {
+  tasks: PropTypes.array
+}
+
+const Task = ({task}) => {
+  return (
+    <>
+    <div className="task">
+
+    </div>
+    </>
+  )
+}
+
+const taskList = [
+  {
+    id: 1,
+    name: 'ta'
+  },
+  {
+    id: 2,
+    name: 'ta'
+  },
+  {
+    id: 3,
+    name: 'ta'
+  },
+  {
+    id: 4,
+    name: 'ta'
+  },
+  {
+    id: 5,
+    name: 'ta'
+  },
+]
+
+ReactDOM.render(
+  <TrelloBoard tasks={taskList}/>,
+  document.querySelector('#root')
+)
